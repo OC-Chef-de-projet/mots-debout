48fefd87313a2de5c66001afe3cbde823b81d6b6
Pierre-Sylvain Augereau <ps.augereau@gmail.com>
2017-07-30 22:08:51 +0200

    Fixture User

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pierre-Sylvain
+ * Date: 30-07-17
+ * Time: 22:04
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+
+
+class LoadUserData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setName('Administrateur');
+        $userAdmin->setEmail('admin@test.com');
+        $userAdmin->setPassword('test');
+        $userAdmin->setRole('ROLE_ADMIN');
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
+}
