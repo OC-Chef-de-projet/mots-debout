961ab134c1b15aa4c688535c55a9921f39d9a681
Pierre-Sylvain Augereau <ps.augereau@gmail.com>
2017-08-07 19:36:08 +0200

    Refactorisation admin

---
+++ src/AppBundle/Controller/Admin/UserController.php
@@ -0,0 +1,113 @@
+<?php
+namespace AppBundle\Controller\Admin;
+use AppBundle\Entity\User;
+use AppBundle\Form\Type\UserType;
+use AppBundle\Form\Type\UserEditType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{
+   
+        /**
+     * Lists all user entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('AppBundle:User')->findAll();
+
+        return $this->render('@AdminUser/index.html.twig', array(
+            'users' => $users,
+        ));
+    }
+
+    /**
+     * Creates a new user entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+			// Encode the new users password
+            $encoder = $this->get('security.password_encoder');
+            $password = $encoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // Save
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_user_index');
+        }
+
+        return $this->render('@AdminUser/new.html.twig', array(
+            'user' => $user,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing user entity.
+     *
+     */
+    public function editAction(Request $request, User $user)
+    {
+        $deleteForm = $this->createDeleteForm($user);
+        $editForm = $this->createForm(UserEditType::class, $user);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('admin_user_index');
+        }
+
+        return $this->render('@AdminUser/edit.html.twig', array(
+            'user' => $user,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a user entity.
+     *
+     */
+    public function deleteAction(Request $request, User $user)
+    {
+        $form = $this->createDeleteForm($user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($user);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_user_index');
+    }
+
+    /**
+     * Creates a form to delete a user entity.
+     *
+     * @param User $user The user entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(User $user)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_user_delete', array('id' => $user->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
+
---
+++ src/AppBundle/Form/Type/UserEditType.php
@@ -0,0 +1,47 @@
+<?php
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('email', EmailType::class,[
+                'label' => 'Adresse mail',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôles',
+                'attr' => ['class' => ''],
+                "required" => true,
+                'multiple' => true,
+                'expanded' => true, // render check-boxes
+                'choices' => [
+                    'Administrateur' => 'ROLE_ADMIN',
+                    'Editeur' => 'ROLE_EDITOR',
+                    'Contributeur' => 'ROLE_CONTRIBUTOR'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\User',
+            'validation_groups' => array('edit'),
+        ]);
+    }
+}
