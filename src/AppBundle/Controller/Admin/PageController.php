546075971c3fb2de02fcfdfc24b0a9bf576b7366
Pierre-Sylvain Augereau <ps.augereau@gmail.com>
2017-10-14 09:49:00 +0200

    refactor (Pages): duplicate code

--- src/AppBundle/Controller/Admin/PageController.php
+++ src/AppBundle/Controller/Admin/PageController.php
@@ -24,38 +24,5 @@ class PageController extends Controller
             'pages' => $pages,
         ]);
     }
-
-
-    /**
-     * Displays a form to edit an existing post entity.
-     *
-     */
-    public function editAction(Request $request, Post $post, UserInterface $user)
-    {
-
-
-        if($post->getImagelink()) {
-            $post->setImageLink(
-                new File('./assets/img/' . $post->getImagelink())
-            );
-        }
-        $PostService = $this->get('service_post');
-        $form = $this->createForm(PostType::class, $post, ['status' => $PostService->getStatusOptions($user, $post)]);
-        $form->handleRequest($request);
-
-        $deleteForm = $this->createDeleteForm($post);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->container->get('service_post')->savePost($post, $form);
-            return $this->redirectToRoute('admin_post_index');
-        }
-
-        return $this->render('@AdminPost/edit.html.twig', [
-            'title' => $this->container->get('service_post')->getActionTitle($user, $post),
-            'post' => $post,
-            'form' => $form->createView(),
-            'delete_form' => $deleteForm->createView()
-        ]);
-    }
 }
 
