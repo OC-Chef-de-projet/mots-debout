599b030c04dee12fab50d6aabc815216b312600c
Pierre-Sylvain Augereau <ps.augereau@gmail.com>
2018-02-20 19:08:20 +0100

    chore (*) Refactor directory structure and files name

--- src/AppBundle/Controller/EtmdController.php
+++ src/AppBundle/Controller/EtmdController.php
@@ -12,4 +12,21 @@ class EtmdController extends Controller
     {
         return $this->render('etmd/index.html.twig');
     }
+
+    public function AteliersAction()
+    {
+        return $this->render('etmd/ateliers.html.twig',[
+            'trainings' => $this->getDoctrine()->getManager()->getRepository('AppBundle:Training')->upcoming()
+        ]);
+    }
+
+    public function cours_collectifsAction()
+    {
+        return $this->render('etmd/cours_collectifs.html.twig');
+    }
+
+    public function cours_particuliersAction()
+    {
+        return $this->render('etmd/cours_particuliers.html.twig');
+    }
 }
